36bfd535cbb9141254c3ec1c1092242ef8e1399f
omelette1 <stale1735@gmail.com>
2024-04-07 21:24:05 -0700

    update

--- src/pages/Project/project.js
+++ src/pages/Project/project.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Link, Outlet, Route, Routes } from "react-router-dom";
+import "./project.css"; // Import CSS file for styling
 
 export default function ProjectsLayout() {
   return (
     <>
-      <a href="https://main--outew.netlify.app/">Memory game</a>
+      <div className="button-box">
+        <Link to="https://main--outew.netlify.app/">Memory game</Link>
+      </div>
+      <div className="button-box">
+        <Link to="https://6613310e4a79d11a684879c8--asddsd.netlify.app/">Bootstrap</Link>
+      </div>
+      <div className="button-box">
+        <Link to="https://66133c8382510723ee02e79c--sadasd12.netlify.app/">Weather App</Link>
+      </div>
     </>
   );
 }
@@ -12,6 +21,8 @@ export default function ProjectsLayout() {
 export function Projects() {
   return (
     <Routes>
+      {/* Define routes here if needed */}
+      <Route path="/" element={<ProjectsLayout />} />
     </Routes>
   );
 }
